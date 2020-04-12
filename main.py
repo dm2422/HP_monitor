c5f9b7a6dfca9eb973bf018ed292924900b8fc0b
Yz4230 <yuzupon1480@gmail.com>
2020-04-12 09:44:01 +0900

    Production.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from utils import check_update, render_message_text
+from utils import check_update, render_message_text, broadcast
 
-for name, news_list in check_update().items():
-    for news in news_list:
-        # Replace broadcast() in production.
-        print(render_message_text(news, name))
+if __name__ == "__main__":
+    for school_name, news_list in check_update().items():
+        for news in news_list:
+            # Replace broadcast() in production.
+            broadcast(render_message_text(news, school_name))
