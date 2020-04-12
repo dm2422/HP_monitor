75e30508cd33d48a20766ad6d63a16ef176bd9c3
Yz4230 <yuzupon1480@gmail.com>
2020-04-12 10:55:16 +0900

    Fix: Cannot init on first running.

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 from sys import argv
 
+from settings import HISTORY_JSON_PATH
 from utils import check_update
 
 
 def cmd_init():
+    with open(HISTORY_JSON_PATH, "w") as wf:
+        wf.write("{}")
     check_update()
 
 
