e7178553e3aadb3456e0138f249e84739051ae85
Yz4230 <yuzupon1480@gmail.com>
2020-04-13 13:28:22 +0900

    Readable: Make API directory. #1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from line_api import broadcast
-from twitter_api import twitter_api
+from API import line_api
+from API.twitter_api import twitter_api
 from utils import check_update, render_message_text
 
 if __name__ == "__main__":
     for school_name, news_list in check_update().items():
         for news in news_list:
             rendered_text = render_message_text(news, school_name)
-            broadcast(rendered_text)
+            line_api.broadcast(rendered_text)
             twitter_api.update_status(rendered_text)
