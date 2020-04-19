8fc8617de2025cba60629a550b5e9562aa86a731
Yz4230 <yuzupon1480@gmail.com>
2020-04-19 19:34:49 +0900

    Add docstring.

--- loaders.py
+++ loaders.py
@@ -6,6 +6,11 @@ from const_settings import TOKENS_JSON_PATH
 
 
 def load_tokens(tokens_json_path=TOKENS_JSON_PATH) -> Dict[str, TokensSet]:
+    """
+    Load tokens from TOKENS_JSON_PATH.
+    :param tokens_json_path: The path to the JSON thad the tokens are written.
+    :return: Dict[school_name: str, tokens_set: TokensSet]
+    """
     with open(tokens_json_path, "r", encoding="utf-8") as rf:
         tokens_ = json.load(rf)
 
