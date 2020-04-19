e8bf02826cd3920edbdcc15050829c49b3a55386
Yz4230 <yuzupon1480@gmail.com>
2020-04-19 12:13:40 +0900

    Improve: Support "use_shared" and "null" options for LINE tokens.

--- API/line_api.py
+++ API/line_api.py
@@ -1,11 +1,26 @@
+from typing import Optional
+
 import requests
 
+from API.structs import TokenOptionsEnum, Line
 from crawlers.common import News
 from settings import TOKENS
 from utils import render_text_default
 
 
+def get_line_tokens(school_name: str) -> Optional[Line]:
+    tokens = TOKENS[school_name].line
+    if tokens == TokenOptionsEnum.USE_SHARED:
+        tokens = TOKENS["shared"].line
+
+    assert not isinstance(tokens, TokenOptionsEnum)
+    return tokens
+
+
 def broadcast(news: News, school_name: str) -> None:
+    line_tokens = get_line_tokens(school_name)
+    if not line_tokens:
+        return
     rendered_text = render_text_default(news, school_name)
     api_url = "https://api.line.me/v2/bot/message/broadcast"
 
@@ -19,7 +34,7 @@ def broadcast(news: News, school_name: str) -> None:
     }
 
     headers = {
-        "Authorization": "Bearer " + TOKENS[school_name].line.channel_token,
+        "Authorization": "Bearer " + line_tokens.channel_token,
         "Content-Type": "application/json"
     }
 
