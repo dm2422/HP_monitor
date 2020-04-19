22d9819053b1635b10644ff2b2782084afc90ec9
Yz4230 <yuzupon1480@gmail.com>
2020-04-19 12:09:43 +0900

    Improve: Support "use_shared" and "null" options for Twitter tokens.

--- API/twitter_api.py
+++ API/twitter_api.py
@@ -1,14 +1,27 @@
 import copy
+from typing import Optional
 
 import tweepy
 
+from API.structs import Twitter, LineEnum
 from crawlers.common import News
 from settings import TOKENS
 from utils import render_twitter_text
 
 
+def get_twitter_tokens(school_name: str) -> Optional[Twitter]:
+    tokens = TOKENS[school_name].twitter
+    if tokens == LineEnum.USE_SHARED:
+        tokens = TOKENS["shared"].twitter
+
+    assert not isinstance(tokens, LineEnum)
+    return tokens
+
+
 def broadcast(news: News, school_name: str) -> None:
-    twitter_tokens = TOKENS[school_name].twitter
+    twitter_tokens = get_twitter_tokens(school_name)
+    if not twitter_tokens:
+        return
     auth = tweepy.OAuthHandler(
         twitter_tokens.consumer_key,
         twitter_tokens.consumer_secret
