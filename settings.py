cfddddf7615f88bebee9d03e034136ec4d9d5891
Yz_4230 <yuzupon1480@gmail.com>
2020-04-22 10:46:33 +0900

    Fix type hint.

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 import json
 from functools import lru_cache
-from typing import Dict
+from typing import Dict, Union
 
 from const_settings import TOKENS_JSON_PATH
 
 
 @lru_cache
-def load_tokens(tokens_json_path=TOKENS_JSON_PATH) -> Dict[str, Dict[str, str]]:
+def load_tokens(tokens_json_path=TOKENS_JSON_PATH) -> Dict[str, Dict[str, Union[Dict[str, str], str, None]]]:
     """
     Load tokens from TOKENS_JSON_PATH.
     :param tokens_json_path: The path to the JSON thad the tokens are written.
