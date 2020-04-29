ca8a478c975da814069a4ed4b83c2ee0f6f1fc2e
Yz4230 <yuzupon1480@gmail.com>
2020-04-29 20:58:12 +0900

    Add: "run-once" & "monitor -i <sec>" commands.

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 import argparse
 import time
-from typing import Callable, Dict
+from typing import Callable, Dict, Tuple
 
 from const_settings import HISTORY_JSON_PATH
 from utils import save_json_default
 
-ACTION_TO_FUNC: Dict[str, Callable] = {}
+ACTION_TO_FUNC: Dict[str, Tuple[Callable, Tuple[str]]] = {}
 
 
-def command_action(command_name: str) -> Callable[[Callable], Callable]:
+def command_action(command_name: str, *expected_args: str) -> Callable[[Callable], Callable]:
     def annotation(func: Callable) -> Callable:
-        ACTION_TO_FUNC[command_name] = func
+        ACTION_TO_FUNC[command_name] = (func, expected_args)
         return func
 
     return annotation
@@ -53,16 +53,57 @@ def history():
         print(f"{site} has {len(cache)} history.")
 
 
+@command_action("run-once")
+def run_once():
+    from utils import initialize_logger
+    from shortcuts import check_update, broadcast_all
+
+    initialize_logger()
+    for news in check_update():
+        broadcast_all(news)
+
+
+@command_action("monitor", "interval")
+def monitor(interval: int):
+    from utils import initialize_logger
+    from shortcuts import check_update, broadcast_all
+
+    initialize_logger()
+    while True:
+        for news in check_update():
+            broadcast_all(news)
+        time.sleep(interval)
+
+
+def execute_command(args: argparse.Namespace) -> int:
+    func_args = {}
+    target_command = ACTION_TO_FUNC[args.action]
+    target_func = target_command[0]
+    target_func_args = target_command[1]
+    for a in target_func_args:
+        func_args[a] = getattr(args, a)
+    return target_func(**func_args) or 0
+
+
 if __name__ == "__main__":
     import logging
 
     logging.basicConfig(level=logging.INFO)
+
     parser = argparse.ArgumentParser(description="Manage this monitor.")
     parser.add_argument("action", choices=ACTION_TO_FUNC.keys())
-    command_args = parser.parse_args()
+    parser.add_argument("-d", "--debug", default=False, action="store_true", help="enable debug mode")
+    parser.add_argument("-i", "--interval", default=60, help="update interval in seconds", type=int)
+    commandline_args = parser.parse_args()
+
+    if getattr(commandline_args, "debug", False):
+        logging.getLogger().setLevel(logging.DEBUG)
+        logging.debug("Debug mode is enabled!")
 
     start_time = time.time()
-    ACTION_TO_FUNC[command_args.action]()
+    exit_code = execute_command(commandline_args)
     elapsed = time.time() - start_time
 
-    print(f"The command: {repr(command_args.action)} has finished in {round(elapsed, 2)} seconds.")
+    print(f"The command: {repr(commandline_args.action)} has finished "
+          f"with exit code {exit_code} "
+          f"in {round(elapsed, 2)} seconds.")
